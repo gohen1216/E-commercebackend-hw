be3af53818ff34216b349bcbd3c0b1f382150cd4
Sanusi Cole <sanusi1612@icloud.com>
2021-09-23 19:08:50 -0400

    adds accociations

--- 02-Homework/Develop/models/index.js
+++ 02-Homework/Develop/models/index.js
@@ -5,12 +5,31 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+ 
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongToMany(Tag, {
+  foreignKey: 'tag_id',
+  through: ProductTag
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongToMany(Product, {
+  foreignKey: 'product_id',
+  through: ProductTag
+});
+
+// A reader can have many books
+
+
+// A book belongs to a single reader
+
+
 
 module.exports = {
   Product,
